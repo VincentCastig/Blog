85fb9a485200500fa80741925921801a870420d0
VincentCastig <vincent.castig@gmail.com>
2017-12-20 19:33:07 -0600

    Added eslint

---
+++ blog/src/Header/Header.js
@@ -0,0 +1,14 @@
+import component from 'react';
+import '../CSS/creative.min.css'; 
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <p>ok</p>
+            </div>
+        );
+    }
+}
+
+export default Header
