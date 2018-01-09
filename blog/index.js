6bccbc32a3c6a5e62a013110d94ca4ab5b24caa5
VincentCastig <vincent.castig@gmail.com>
2018-01-09 10:24:32 -0600

    Connected PostgreSQL

--- blog/index.js
+++ blog/index.js
@@ -1,27 +1,21 @@
-// import Axios from 'axios';
+
 const dotenv = require('dotenv').config();
 const express = require('express');
 const { json } = require('body-parser');
-// const massive = require('massive');
 const cors = require('cors');
-// const path = require('path');
 const port = 3000
 
 const userCtrl = require('./public/Ctrl/controller');
-// const loginCtrl = require('./ctrl/loginCtrl');
 
 const app = module.exports = express();
 app.use(json());
 app.use(cors());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 const connectionString = process.env.DATABASE_URL; //Connects to heroku bro
 console.log('connetion', connectionString)
 massive(connectionString).then(db => {app.set('db', db)});
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(express.static('./public'));
+
 
 app.post('/postArticle', userCtrl.post_article);
 app.get('/getAllArticles', userCtrl.get_all_articles);
