11e4c3c5da1e62eb8d46ae3afbf7c982b751e594
VincentCastig <vincent.castig@gmail.com>
2018-01-09 09:57:30 -0600

    Connected Redux

--- blog/src/Components/home.js
+++ blog/src/Components/home.js
@@ -1,31 +1,31 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { fetchArticles } from '../actions';
+import { connect } from 'react-redux';
+import { fetchArticles } from '../actions';
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
-    // renderArticleList() {
-    //     return _.map(this.props.articles, (article) => {
-    //         return (
-    //         <li key={article.id} className="list-group-item">
-    //             <a><h2><Link to={`post/${article.id}`}>{article.title}</Link></h2></a>
-    //             <img class="articleImage" src={(article.content)} />
-    //         </li>
-    //         )
-    //     })
-    // }
+    renderArticleList() {
+        return _.map(this.props.articles, (article) => {
+            return (
+            <li key={article.id} className="list-group-item">
+                <a><h2><Link to={`post/${article.id}`}>{article.title}</Link></h2></a>
+                <img class="articleImage" src={(article.content)} />
+            </li>
+            )
+        })
+    }
 
-    // componentDidMount() {
-    //     this.props.fetchArticles();
-    // }
+    componentDidMount() {
+        this.props.fetchArticles();
+    }
 
     render() {
         // console.log(this.props.articles);
         return (
             <div className="listContainer">
                 <ul>
-                    {/* {this.renderArticleList()} */}
+                    {this.renderArticleList()}
                     <li>Home</li>
                 </ul>
             </div>
@@ -33,10 +33,10 @@ class Home extends Component {
     }
 }
 
-// function mapStateToProps(state) {
-//     console.log('state', state);
-//     return { articles: state.articles};
-// }
+function mapStateToProps(state) {
+    console.log('state', state);
+    return { articles: state.articles};
+}
 
-export default Home;
-// export default connect(mapStateToProps, { fetchArticles })(Home);
+// export default Home;
+export default connect(mapStateToProps, { fetchArticles })(Home);
