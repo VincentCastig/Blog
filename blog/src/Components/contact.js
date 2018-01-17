3fe4ef5145df163ef1ac3680cdf5821c3910c5ad
VincentCastig <vincent.castig@gmail.com>
2018-01-17 15:59:44 -0600

    Contact with Nodemailer

--- blog/src/Components/contact.js
+++ blog/src/Components/contact.js
@@ -1,9 +1,105 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Label } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { postEmail } from '../actions';
 
-const Contact = () => {
-    return (
-        <h1>Welcome to the Contact Page</h1>
-    )
+class Contact extends Component {
+    renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className =  `form-group ${touched && error ? 'has-danger' : ''}`
+
+        return (
+        <div className={className}>
+            <Label>{field.label}</Label>
+            <input
+                className="form-control"
+                type="text"
+                {...field.input} 
+            />
+            <div className="text-help">
+                {touched ? error: ''}
+            </div>
+        </div>
+        );
+    }
+    
+    onSubmit(values) {
+      console.log('values', values)
+        this.props.postEmail(values, () => {
+            this.props.history.push('/');
+        });
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="">
+              <div className="name-field">
+                <Field 
+                    label="Name"
+                    name="name"
+                    component={this.renderField}
+                />
+              </div>
+              <div className="email-field">
+                <Field 
+                    label="Email"
+                    name="email"
+                    component={this.renderField}
+                />
+              </div>
+                <div className="message-content">
+                <Field 
+                    label="Comment"
+                    name="message"
+                    component={this.renderField}
+                />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+
+                <div>
+      { window.location.hash === '#success' &&
+        <div id="success">
+          <p>Your message has been sent!</p>
+        </div>
+      }
+      { window.location.hash === '#error' &&
+        <div id="error">
+          <p>An error occured while submitting the form.</p>
+        </div>
+      }
+    </div>
+            </form>
+        );
+    }
 }
 
-export default Contact;
+function validate(values) {
+    //console.log(values -> {title: 'asdf', categories: 'qwerty'})
+    const errors = {}
+
+    //validate the inputs from the values
+    if (!values.name) {
+        errors.name = "Enter a Name"
+    }
+    if (!values.email) {
+        errors.email = "Enter Your Email"
+    }
+    if (!values.message) {
+        errors.message = "Enter Your Message"
+    }
+    //if errors is empty, the form is fine to submit
+    //if errors has any properties, the form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsEmailForm'
+})(
+    connect(null, { postEmail })(Contact)
+);
--- blog/src/Components/home.js
+++ blog/src/Components/home.js
@@ -2,23 +2,32 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchArticles } from '../actions';
+// import SearchBar from './search-bar'
 import { Link } from 'react-router-dom';
 import { Grid, Col, Media } from 'react-bootstrap';
 
 class Home extends Component {
     renderArticleList() {
         
-        return _.map(this.props.articles, (article) => {
-            const time = article.date.substring(0,10);
-            return (
-            <Link to={`post/${article.id}`} key={article.id} className="list-group-item">
-                <a><h2><Link to={`post/${article.id}`}>{article.title}</Link></h2></a>
-                <p>{time}</p>
-                <img class="articleImage" src={(`${article.image}`)} />
-            </Link>
-            )
+        const articles = _.map(this.props.articles, (article) => {
+            // const time = article.date.substring(0,10);
+            return article;
         })
-    }
+        const reverseArticles = articles.reverse();
+
+        return reverseArticles.map(article => {
+            const time = article.date.substring(0,10);
+            if(article.id > reverseArticles.length - 5) {
+                return (
+                    <Link to={`${article.id}`} key={article.id} className="list-group-item">
+                        <a><h2><Link to={`${article.id}`}>{article.title}</Link></h2></a>
+                        <h5>{time}</h5>
+                        <img class="articleImage" src={(`${article.image}`)} />
+                    </Link>
+                )
+            }
+            })
+        }
 
     componentDidMount() {
         this.props.fetchArticles();
@@ -27,8 +36,8 @@ class Home extends Component {
     render() {
         // console.log(this.props.articles);
         return (
-            
-                <Col md={9} className="list-group">
+                
+                <Col md={12} className="list-group">
                     <ul className="listContainer">
                         {this.renderArticleList()}
                     </ul>
@@ -39,7 +48,6 @@ class Home extends Component {
 }
 
 function mapStateToProps(state) {
-    console.log('state', state);
     return { articles: state.articles};
 }
 
