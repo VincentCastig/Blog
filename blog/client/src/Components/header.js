e74f90ca1823a86ad62351269ff81ae5cb6d1602
VincentCastig <vincent.castig@gmail.com>
2018-03-26 15:39:00 -0500

    Navbar Collapse

--- blog/client/src/Components/header.js
+++ blog/client/src/Components/header.js
@@ -1,30 +1,85 @@
 import React, { Component } from 'react';
-import { NavDropdown, MenuItem } from 'react-bootstrap';
+import { NavDropdown, MenuItem, Navbar, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Dropdown from 'react-dropdown';
+
 
 class Header extends Component {
+    constructor(props) {
+      super(props)
+      this.navToggle = this.navToggle.bind(this);
+      this.categoryToggle = this.categoryToggle.bind(this);
+      this.toggleBoth = this.toggleBoth.bind(this);
+
+    }
+    navToggle = () => { 
+      console.log('clicked')
+      let linksEl = document.querySelector('.collapse') 
+      if (linksEl.style.display === 'block') {
+        linksEl.style.display = 'none';
+      } else {
+        linksEl.style.display = 'block';
+      }
+    };
+    categoryToggle = () => { 
+      console.log('category')
+      let linksCat = document.querySelector('.dropdown-menu') 
+      if (linksCat.style.display === 'block') {
+        linksCat.style.display = 'none';
+      } else {
+        linksCat.style.display = 'block';
+      }
+    };
+    toggleBoth = () => {
+      this.categoryToggle();
+      this.navToggle();
+    }
     render() {
       
-
         return (
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">VincentCastigliola.com</h1>
-          <a><Link to="/">Home</Link></a>
-          <a><NavDropdown eventKey="1" title="Category" id="basic-nav-dropdown">
-            <MenuItem><Link to="Programming">Programming</Link></MenuItem>
-            <MenuItem><Link to="Fitness">Fitness</Link></MenuItem>
-            <MenuItem><Link to="Cooking">Cooking</Link></MenuItem>
-            <MenuItem><Link to="BookReviews">Book Reviews</Link></MenuItem>
-          </NavDropdown>
-          </a>
-          <a><Link to="About">About</Link></a>
-          <a><Link to="Contact">Contact</Link></a>
+       
+        <Navbar collapseOnSelect>
+		<Navbar.Header>
+			<Navbar.Brand>
+				<Link to="/">VincentCastigliolaIII.com</Link>
+			</Navbar.Brand>
+			<Navbar.Toggle />
+		</Navbar.Header>
+		<Navbar.Collapse>
+			
+			<Nav pullRight>
+      
+				<NavItem eventKey={2} href="#">
+        {/* <Link to="/">Home</Link> */}
+				</NavItem>
+				<NavDropdown eventKey={3} onClick={this.categoryToggle} title="Categories" id="basic-nav-dropdown">
+             <MenuItem><Link to="Programming" onClick={this.toggleBoth}>Programming</Link></MenuItem>
+             <MenuItem><Link to="Fitness" onClick={this.toggleBoth}>Fitness</Link></MenuItem>
+             <MenuItem><Link to="Cooking" onClick={this.toggleBoth}>Cooking</Link></MenuItem>
+             <MenuItem><Link to="BookReviews" onClick={this.toggleBoth}>Book Reviews</Link></MenuItem>
+					
+				</NavDropdown>
+				<NavItem eventKey={1} href="#">
+        <Link to="About" onClick={this.navToggle}>About</Link>
+				</NavItem>
+				<NavItem eventKey={2} href="#">
+        <Link to="Contact" onClick={this.navToggle}>Contact</Link>
+				</NavItem>
+			</Nav>
+		</Navbar.Collapse>
+    
+    <div className="navNarrow">
+        <button type='button' className="" onClick={this.navToggle}>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+        </button>
           
-        </header>
+    </div>
+	</Navbar>
         );
+        
     }
+    
 }
 
 export default Header;
